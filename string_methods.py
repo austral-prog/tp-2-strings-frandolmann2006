c8c69adcae2a3f639acff2e3fc97e84ecc664df4
frandolmann2006 <frankdolmann@gmail.com>
2026-03-18 17:41:34 -0300

    Update string_methods.py

--- string_methods.py
+++ string_methods.py
@@ -8,3 +8,30 @@ def string_methods():
     multilinea = """Linea 1
     Linea 2
     Linea 3"""
+
+    print(nombre.strip())
+    print(nombre.lstrip())
+    print(nombre.rstrip())
+
+    print(frase.upper())
+    print(frase.lower())
+    print(frase.title())
+
+    print(f"find: {frase.find('gran')}")
+    print(f"replace: {frase.replace('programacion', 'desarrollo')}")
+
+    print(f"count: {frase.count('a')}")
+
+    print(f"contiene Python: {'Python' in frase}")
+    print(f"contiene Java: {'Java' in frase}")
+
+    python = frase[0:6]
+    print(f"slice: {python}")
+
+    print(f"paso: {python[::2]}")
+
+    print(python[::-1])
+
+    print(f"Formato: {nombre.strip()} sabe Python")
+
+    print(multilinea)
