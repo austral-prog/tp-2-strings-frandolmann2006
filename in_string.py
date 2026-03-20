d0cfdf0eccb1b0709f7a56eb819de3e20a820ff1
frandolmann2006 <frankdolmann@gmail.com>
2026-03-20 18:48:11 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -7,8 +7,8 @@ def check_vowels():
     nombre = input("introduzca su nombre: ")
     print(nombre.lower())
 
-    print(f"contiene a:{'a' in nombre}")
-    print(f"contiene e:{'e' in nombre}")
-    print(f"contiene i:{'i' in nombre}")
-    print(f"contiene o:{'o' in nombre}")
-    print(f"contiene u:{'u' in nombre}")
+    print(f"contiene a: {'a' in nombre}")
+    print(f"contiene e: {'e' in nombre}")
+    print(f"contiene i: {'i' in nombre}")
+    print(f"contiene o: {'o' in nombre}")
+    print(f"contiene u: {'u' in nombre}")
