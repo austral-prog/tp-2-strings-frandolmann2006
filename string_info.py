373c49df9d4583d480dbd423c0245642160256a0
frandolmann2006 <frankdolmann@gmail.com>
2026-03-20 18:55:00 -0300

    Update string_info.py

--- string_info.py
+++ string_info.py
@@ -2,10 +2,11 @@ def string_info():
     """Dada la palabra 'Programacion', imprime su longitud, primera y última letra,
     la palabra repetida 3 veces y decorada con '***'.
     """
-    palabra = "Programacion"
-    print(palabra)
-    print(len(palabra))
-    print(palabra[0])
-    print(palabra[-1])
-    print(palabra*3)
-    print(f"***{palabra}***")
+    palabra =  "Programacion"
+    print(f"Palabra: {palabra}")
+    print(f"Longitud: {len(palabra)}")
+    print(f"Primera letra: {palabra[0]}")
+    print(f"Ultima letra: {palabra[-1]}")
+    print(f"Repetida: {palabra*3}")
+    decorada= f"***{palabra}***"
+    print(f"Decorada: ***{palabra}***")
