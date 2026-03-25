caaa68eb381a85ff3d27b2001cd293fd3f7898d5
frandolmann2006 <frankdolmann@gmail.com>
2026-03-25 13:59:40 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -2,6 +2,6 @@ def slice_advanced():
     """Lee un texto e imprime los caracteres desde la posición 4
     en adelante, tomando uno de cada dos (paso 2).
     """
-    pass
-    texto=input()
+
+    texto=input("escriba un texto: ")
     print(texto[4::2])
